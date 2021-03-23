aab3df4d08e49c123a11dc23bdfb658901d6f40f
Aranaktu <paweh10@gmail.com>
2021-03-23 15:04:13 +0100

    v21.1.1.0

--- lua/libs/live_editor.lua
+++ lua/libs/live_editor.lua
@@ -1,4 +1,9 @@
 --- Main live editor lib
 --- Documentation in -> DOC.MD
 
-Log("TODO live_editor.lua")
+Log("Init live_editor.lua")
+
+--- CreateOfferForPlayer(158023, 243, 5)
+function CreateOfferForPlayer(playerid, teamid, offer_type)
+    cCreateOfferForPlayer(playerid, teamid, offer_type or 5)
+end
