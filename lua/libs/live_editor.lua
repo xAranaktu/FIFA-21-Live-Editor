0bc06a976924e76f463d2531f7e2023b1516f30a
Aranaktu <paweh10@gmail.com>
2021-05-03 12:24:12 +0200

    v21.1.1.4

--- lua/libs/live_editor.lua
+++ lua/libs/live_editor.lua
@@ -7,3 +7,31 @@ Log("Init live_editor.lua")
 function CreateOfferForPlayer(playerid, teamid, offer_type)
     cCreateOfferForPlayer(playerid, teamid, offer_type or 5)
 end
+
+function IsPlayerTransferListed(playerid, teamid)
+    return cIsPlayerTransferListed(playerid, teamid or 0)
+end
+
+function IsPlayerLoanListed(playerid, teamid)
+    return cIsPlayerLoanListed(playerid, teamid or 0)
+end
+
+function AddPlayerToTransferList(playerid, teamid)
+    cAddPlayerToTransferList(playerid, teamid or 0)
+end
+
+function AddPlayerToLoanList(playerid, teamid)
+    cAddPlayerToLoanList(playerid, teamid or 0)
+end
+
+function RemovePlayerFromLoanList(playerid, teamid)
+    cRemovePlayerFromLoanList(playerid, teamid or 0)
+end
+
+function RemovePlayerFromTransferList(playerid, teamid)
+    cRemovePlayerFromTransferList(playerid, teamid or 0)
+end
+
+function RemovePlayerFromLists(playerid, teamid)
+    cRemovePlayerFromLists(playerid, teamid or 0)
+end
--- lua/tests/main_test.lua
+++ lua/tests/main_test.lua
@@ -2,13 +2,20 @@
 Log("Run LUA Test")
 ReloadDB()
 
+
 local bIsInCM = IsInCM();
 assert(type(bIsInCM) == "boolean", "IsInCM not a boolean")
 
 if (bIsInCM) then
     Log("Is In Career Mode")
-    SetTransferBudget(6777656)
 
+    Log("Run the tests as FC Barcelona (ID: 241) Manager ")
+    local user_teamid = GetUserTeamID()
+    
+    Log("user_teamid: " .. user_teamid)
+    assert(type(user_teamid) == "number", "user_teamid not a number")
+
+    SetTransferBudget(6777656)
     local transfer_budget = GetTransferBudget()
     Log("transfer_budget: " .. transfer_budget)
     assert(type(transfer_budget) == "number", "transfer_budget not a number")
@@ -18,13 +25,8 @@ if (bIsInCM) then
     local wage_budget = GetWageBudget()
     Log("wage_budget: " .. wage_budget)
     assert(type(wage_budget) == "number", "wage_budget not a number")
-    assert(wage_budget == 1234567, "wage_budget != 6777656")
+    assert(wage_budget == 1234567, "wage_budget != 1234567")
     
-    local user_teamid = GetUserTeamID()
-    
-    Log("user_teamid: " .. user_teamid)
-    assert(type(user_teamid) == "number", "user_teamid not a number")
-
     local arr_table_names = GetDBTablesNames()
     assert(type(arr_table_names) == "table", "arr_table_names not a table")
     Log("Elements in arr_table_names: " .. #arr_table_names)
@@ -59,6 +61,86 @@ if (bIsInCM) then
     Log("playername2 (158023): " .. playername2)
     assert(type(playername2) == "string", "GetPlayerName(158023) not a string")
     assert(playername2 == "Lionel Messi", "playername2 != Lionel Messi")
+
+    Log("Transfer/Loan List Checks for user player")
+    local listing_checks_playerid = 158023
+    local is_on_transferlist = IsPlayerTransferListed(listing_checks_playerid)
+    assert(type(is_on_transferlist) == "boolean", "is_on_transferlist not a boolean")
+
+    local is_on_loanlist = IsPlayerLoanListed(listing_checks_playerid)
+    assert(type(is_on_loanlist) == "boolean", "is_on_loanlist not a boolean")
+
+    RemovePlayerFromLists(listing_checks_playerid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_playerid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_playerid)
+
+    assert(is_on_loanlist == false, listing_checks_playerid .. " is on loan list after RemovePlayerFromLists called")
+    assert(is_on_transferlist == false, listing_checks_playerid .. " is on transfer list after RemovePlayerFromLists called")
+
+    AddPlayerToTransferList(listing_checks_playerid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_playerid)
+    assert(is_on_transferlist, listing_checks_playerid .. " is not on transfer list after AddPlayerToTransferList called")
+    RemovePlayerFromTransferList(listing_checks_playerid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_playerid)
+    assert(is_on_transferlist == false, listing_checks_playerid .. " is on transfer list after RemovePlayerFromTransferList called")
+
+    AddPlayerToLoanList(listing_checks_playerid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_playerid)
+    assert(is_on_loanlist, listing_checks_playerid .. " is not on loan list after AddPlayerToLoanList called")
+    RemovePlayerFromLoanList(listing_checks_playerid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_playerid)
+    assert(is_on_loanlist == false, listing_checks_playerid .. " is not on loan list after RemovePlayerFromTransferList called")
+    
+    RemovePlayerFromLists(listing_checks_playerid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_playerid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_playerid)
+
+    assert(is_on_loanlist == false, listing_checks_playerid .. " is on loan list after second RemovePlayerFromLists called")
+    assert(is_on_transferlist == false, listing_checks_playerid .. " is on transfer list after second RemovePlayerFromLists called")
+
+    Log("Transfer/Loan List Checks for CPU player")
+    local listing_checks_cpu_playerid = 41
+    local listing_checks_cpu_teamid = GetTeamIdFromPlayerId(listing_checks_cpu_playerid)
+    Log("listing_checks_cpu_playerid: " .. listing_checks_cpu_playerid)
+    Log("listing_checks_cpu_teamid: " .. listing_checks_cpu_teamid)
+
+    RemovePlayerFromLists(listing_checks_cpu_playerid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+
+    assert(is_on_loanlist == false, listing_checks_cpu_playerid .. " is on loan list after RemovePlayerFromLists called")
+    assert(is_on_transferlist == false, listing_checks_cpu_playerid .. " is on transfer list after RemovePlayerFromLists called")
+
+    AddPlayerToTransferList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    AddPlayerToLoanList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    assert(is_on_loanlist, listing_checks_cpu_playerid .. " is not on loan list after AddPlayerToLoanList called")
+    assert(is_on_transferlist, listing_checks_cpu_playerid .. " is not on transfer list after AddPlayerToTransferList called")
+
+    RemovePlayerFromTransferList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    assert(is_on_loanlist, listing_checks_cpu_playerid .. " is not on loan list after RemovePlayerFromTransferList called")
+    assert(is_on_transferlist == false, listing_checks_cpu_playerid .. " is on transfer list after RemovePlayerFromTransferList called")
+
+    AddPlayerToTransferList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    RemovePlayerFromLoanList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    assert(is_on_loanlist == false, listing_checks_cpu_playerid .. " is on loan list after RemovePlayerFromLoanList called")
+    assert(is_on_transferlist, listing_checks_cpu_playerid .. " is on not transfer list after RemovePlayerFromLoanList called")
+    AddPlayerToLoanList(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+
+    RemovePlayerFromLists(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_loanlist = IsPlayerLoanListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+    is_on_transferlist = IsPlayerTransferListed(listing_checks_cpu_playerid, listing_checks_cpu_teamid)
+
+    assert(is_on_loanlist == false, listing_checks_cpu_playerid .. " is on loan list after second RemovePlayerFromLists called")
+    assert(is_on_transferlist == false, listing_checks_cpu_playerid .. " is on transfer list after second RemovePlayerFromLists called")
+
+    Log("Transfer/Loan List Checks Done")
 else 
     Log("Not in Career Mode")
 end
